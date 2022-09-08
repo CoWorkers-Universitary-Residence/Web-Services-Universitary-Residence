5a35e11a8a687434870f16c2891cf42e6f248a1c
William <axeso_william@hotmail.com>
2022-09-08 14:55:00 -0500

    feat: create publication table with all methods

--- src/main/java/com/example/webservices/publications/domain/model/service/PublicationService.java
+++ src/main/java/com/example/webservices/publications/domain/model/service/PublicationService.java
@@ -1,4 +1,16 @@
 package com.example.webservices.publications.domain.model.service;
 
+import com.example.webservices.publications.domain.model.entity.Publication;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public interface PublicationService {
+    List<Publication> getAll();
+    Publication getById(Long publicationId);
+    Publication create(Publication publication);
+    Publication update(Long publicationId, Publication request);
+    ResponseEntity<?> delete(Long publicationId);
+
+    //TODO Relation
 }
--- src/main/java/com/example/webservices/publications/resource/PublicationResource.java
+++ src/main/java/com/example/webservices/publications/resource/PublicationResource.java
@@ -1,4 +1,27 @@
 package com.example.webservices.publications.resource;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PublicationResource {
+    private Long id;
+    private String about;
+    private double price;
+    private double escrow;
+    private String extra_expenses;
+    private String gender;
+    private boolean availability;
+    private int rooms;
+    private String bathrooms;
+    private float width;
+    private float height;
+    private String country;
+    private String city;
+    private String address;
+    private boolean visit;
+    private int views;
+
+    //TODO Relation with owner
 }
