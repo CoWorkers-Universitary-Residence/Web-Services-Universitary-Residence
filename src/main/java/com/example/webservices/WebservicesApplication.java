2664e5aabeb1a80a3bac2af4b9d12f1a626aa51a
Jesús <jesuspacheco0219@gmail.com>
2022-09-07 20:27:14 -0500

    feat(application) : feat add cors configuration

--- src/main/java/com/example/webservices/WebservicesApplication.java
+++ src/main/java/com/example/webservices/WebservicesApplication.java
@@ -2,12 +2,28 @@ package com.example.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WebservicesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebservicesApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/")
+                        .allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 
 }
