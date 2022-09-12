ea4389767785b0565220c6958f8be04badfe48af
Edward240402 <eduard.coar@gmail.com>
2022-09-12 09:49:39 -0500

    feat: Change Configuration name for mapping

--- src/main/java/com/example/webservices/shared/mapping/MappingConfiguration.java
+++ src/main/java/com/example/webservices/shared/mapping/MappingConfiguration.java
@@ -4,10 +4,12 @@ package com.example.webservices.shared.mapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration("enhancedModelMapperConfiguration")
+@Configuration("boncesModelMapperConfiguration")
 public class MappingConfiguration {
     @Bean
     public EnhancedModelMapper modelMapper() {
         return new EnhancedModelMapper();
     }
+    //
+
 }
