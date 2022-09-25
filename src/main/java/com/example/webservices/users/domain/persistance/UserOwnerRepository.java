f6672019d3602e1942d0d3ac53978355272fe2c1
JesussPacheco <jesuspacheco0219@gmail.com>
2022-09-24 20:46:52 -0500

    implement serviceIimpl post , get , put , delete

--- src/main/java/com/example/webservices/users/domain/persistance/UserOwnerRepository.java
+++ src/main/java/com/example/webservices/users/domain/persistance/UserOwnerRepository.java
@@ -4,11 +4,13 @@ import com.example.webservices.users.domain.model.entity.UserOwner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Optional;
 
 @Repository
 public interface UserOwnerRepository extends JpaRepository<UserOwner,Long> {
     Optional<UserOwner> findByEmailAndPassword(String email, String password);
-    Boolean existsByNameAndLastName(String name);
+    Boolean existsByNameAndLastName(String name, @NotNull @NotBlank String lastName);
     Boolean existsByEmail(String email);
 }
